d228c43b924948fbe43694c2b33e80d2c27b1943
msg <msg>
2002-12-14 05:27:53 +0000

    Initial checkin of AmazonPop. The client displays top selling books, music, movies, software and toys.

---
+++ kn_apps/amazonpop/forward_amazon.js
@@ -0,0 +1,112 @@
+// <%@LANGUAGE = JScript %>
+// <%
+
+main(WScript.Arguments);
+
+var g_routerURI = "http://www.topiczero.com:8000/kn/";
+
+function alert(text)
+{
+	WScript.echo(text);
+}
+
+function loadXML(url)
+{
+	var dom = WScript.CreateObject("MSXML.DOMDocument");
+	dom.async = false;
+	dom.load(url);
+	return dom;
+}
+
+function loadSales(url)
+{
+	var feed = loadXML(url);
+	return feed.selectNodes("/ProductInfo/Details");
+}
+
+function forwardSales(topic,items)
+{
+	var poster = WScript.CreateObject("Microsoft.XMLHTTP");
+	var item;
+	var title;
+	var author="Ms. Anona";
+	var link;
+	var rank;
+	var body;
+
+	var content;
+
+	for (var i=0; i < items.length; i++)
+	{
+		item = items[i];
+		
+		title = item.selectSingleNode("ProductName");
+		if (title)
+			title = title.text;
+
+		rank = item.selectSingleNode("SalesRank");
+		if (rank)
+			rank = rank.text;
+		else
+			rank = "none";
+
+		author = item.selectSingleNode("Authors/Author");
+		if (author)
+			author = author.text;			
+		else
+			author = "";
+
+		link = item.selectSingleNode("@url");
+		if (link)
+			link = link.text;
+			
+		if (title && link)
+		{
+			var reg;
+
+			poster.open("POST",g_routerURI,false);
+			body = "do_method=notify"+
+//			"&kn_expires=%2b36000"+
+			"&type=status"+
+			"&kn_to="+topic+
+			"&title="+title+
+			"&rank="+rank+
+			"&author="+author+
+			"&link="+encodeURI(link);
+			body = body.replace(/\s/g,"+");
+		poster.setRequestHeader("Content-Type","application/x-www-form-urlencoded");		
+		poster.setRequestHeader("Content-Length",body.length);		
+			poster.send(body+"\n\n" );
+			poster.responseText;
+if (poster.status == '400')
+{
+//	alert(poster.status + " " +link);
+//	alert(body);
+}
+		}
+	}
+		
+}
+
+function forward(pages,categoryId,topic)
+{
+	var sales;
+	for (var i=1; i <= pages; i++)
+	{
+		sales=loadSales("http://xml.amazon.com/onca/xml2?t=webservices-20&dev-t=D2W7SBNOI2U2ML&BrowseNodeSearch="+categoryId+"&mode=books&type=heavy&page="+i+"&f=xml&sort=+salesrank");
+		forwardSales(topic,sales);
+	}	
+}
+
+function main(args)
+{
+	// books, top selling=1000
+	forward(4,1000,"/amazon.com/sales/books/");
+	forward(4,301668,"/amazon.com/sales/music/");
+	forward(4,404272,"/amazon.com/sales/movies/");
+	forward(4,130,"/amazon.com/sales/movies/");
+	forward(4,491286,"/amazon.com/sales/software/");
+	forward(4,491290,"/amazon.com/sales/toys/");
+}	
+
+// %>
