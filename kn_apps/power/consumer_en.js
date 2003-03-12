c8364caa1f57b9aac8cbbc0db2b1bd0cb0e0ae42
ifindkarma <ifindkarma>
2003-03-12 03:19:32 +0000

    Added the PubSub Power Demo application.

---
+++ kn_apps/power/consumer_en.js
@@ -0,0 +1,83 @@
+// Copyright 2000-2003 KnowNow, Inc.  All Rights Reserved.
+//
+// @KNOWNOW_LICENSE_START@
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in
+// the documentation and/or other materials provided with the
+// distribution.
+// 
+// 3. The name "KnowNow" is a trademark of KnowNow, Inc. and may not
+// be used to endorse or promote any product without prior written
+// permission from KnowNow, Inc.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL KNOWNOW, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// @KNOWNOW_LICENSE_END@
+//
+// $Id: consumer_en.js,v 1.1 2003/03/12 03:19:33 ifindkarma Exp $
+//
+// This localization template was automatically generated using
+// kn_localize.sh.  The numbers inside /* comments */ are line numbers
+// in the original file.  Change "en" to the standard code for your
+// target language, and replace the nulls with strings in the target
+// language.
+//
+// Generated: Wed May 23 13:43:04 PDT 2001
+/* 40 */ kn_localize("en","",
+"Rolling Blackouts Are In Effect",
+null);
+/* 44 */ kn_localize("en","",
+"email:",
+null);
+/* 52 */ kn_localize("en","",
+"Notification Preferences",
+null);
+/* 62 */ kn_localize("en","",
+'Notify me when %{0}primary%{1} systems are affected.',
+null);
+/* 79 */ kn_localize("en","",
+'Notify me when %{0}secondary%{1} systems are affected.',
+null);
+/* 117 */ kn_localize("en","",
+"%{0} [Can't Route To Mailer %{1} %{2}]",
+null);
+/* 123 */ kn_localize("en","",
+"%{0} [Can't Notify %{1} %{2}]",
+null);
+/* 161 */ kn_localize("en","",
+'Primary Systems Are Offline',
+null);
+/* 162 */ kn_localize("en","",
+'Primary Systems Are Online',
+null);
+/* 196 */ kn_localize("en","",
+'Secondary Systems Are Online',
+null);
+/* 197 */ kn_localize("en","",
+'Secondary Systems Are Offline',
+null);
+/* 237 */ kn_localize("en","",
+"Comic Sans MS",
+null);
+/* 239 */ kn_localize("en","",
+'Electricity is now %{0}&#162; per kilowatt-hour.',
+null);
+// End of consumer_en.js
---
+++ kn_apps/power/producer_en.js
@@ -0,0 +1,62 @@
+// Copyright 2000-2003 KnowNow, Inc.  All Rights Reserved.
+//
+// @KNOWNOW_LICENSE_START@
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in
+// the documentation and/or other materials provided with the
+// distribution.
+// 
+// 3. The name "KnowNow" is a trademark of KnowNow, Inc. and may not
+// be used to endorse or promote any product without prior written
+// permission from KnowNow, Inc.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL KNOWNOW, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// @KNOWNOW_LICENSE_END@
+//
+// $Id: producer_en.js,v 1.1 2003/03/12 03:19:33 ifindkarma Exp $
+//
+// This localization template was automatically generated using
+// kn_localize.sh.  The numbers inside /* comments */ are line numbers
+// in the original file.  Change "en" to the standard code for your
+// target language, and replace the nulls with strings in the target
+// language.
+//
+// Generated: Wed May 23 13:43:02 PDT 2001
+/* 74 */ kn_localize("en","",
+'Price: %{0} per kWh',
+null);
+/* 89 */ kn_localize("en","",
+"Rolling Blackouts Are In Effect",
+null);
+/* 96 */ kn_localize("en","",
+"%{0}\xA2",
+null);
+/* 103 */ kn_localize("en","",
+"Increase Price",
+null);
+/* 104 */ kn_localize("en","",
+"Decrease Price",
+null);
+/* 150 */ kn_localize("en","",
+"Power Exchange Committee Alleges Price-Fixing",
+null);
+// End of producer_en.js
