f64607b113ca4dae1d6a742d779a24e519b05be4
ifindkarma <ifindkarma>
2003-03-22 04:06:22 +0000

    Added stock portfolio with ticker.  Uses mod_pubsub/kn_sense/nasdaq.plx .

---
+++ kn_apps/stocks1/index_en.js
@@ -0,0 +1,94 @@
+// 
+//  Copyright 2000-2003 KnowNow, Inc.  All Rights Reserved.
+// 
+// @KNOWNOW_LICENSE_START@
+//  
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//  
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in
+// the documentation and/or other materials provided with the
+// distribution.
+//  
+// 3. The name "KnowNow" is a trademark of KnowNow, Inc. and may not
+// be used to endorse or promote any product without prior written
+// permission from KnowNow, Inc.
+//  
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL KNOWNOW, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+// @KNOWNOW_LICENSE_END@
+// 
+// $Id: index_en.js,v 1.1 2003/03/22 04:06:24 ifindkarma Exp $
+// 
+// 
+// This localization template was automatically generated using
+// kn_localize.sh.  The numbers inside /* comments */ are line numbers
+// in the original file.  Change "en" to the standard code for your
+// target language, and replace the nulls with strings in the target
+// language.
+//
+// Generated: Fri Nov 30 02:35:20 PST 2001
+/* 90 */ kn_localize("en","",
+"Bid",
+null);
+/* 91 */ kn_localize("en","",
+"Ask",
+null);
+/* 92 */ kn_localize("en","",
+"Volume",
+null);
+/* 93 */ kn_localize("en","",
+"Last",
+null);
+/* 94 */ kn_localize("en","",
+"Close",
+null);
+/* 326 */ kn_localize("en","",
+'font-family: arial, helvetica, sans-serif; font-size: 12px;',
+null);
+/* 329 */ kn_localize("en","",
+'font-family: arial, helvetica, sans-serif; font-size: 18px;',
+null);
+/* 332 */ kn_localize("en","",
+'font-family: arial, helvetica, sans-serif; font-size: 10px;',
+null);
+/* 341 */ kn_localize("en","",
+"%{0}'s Stock Portfolio",
+null);
+/* 348 */ kn_localize("en","",
+"Ticker Symbol",
+null);
+/* 373 */ kn_localize("en","",
+"Add",
+null);
+/* 377 */ kn_localize("en","",
+"Remove",
+null);
+/* 395 */ kn_localize("en","",
+"This page uses delayed data from %{0}.",
+null);
+/* 398 */ kn_localize("en","",
+"nasdaq.com",
+null);
+/* 401 */ kn_localize("en","",
+"Prices are in US dollars.",
+null);
+/* 416 */ kn_localize("en","",
+"Stock Portfolio",
+null);
+// End of index_en.js
---
+++ kn_apps/stocks1/index_ja.js
@@ -0,0 +1,92 @@
+// 
+//  Copyright 2000-2003 KnowNow, Inc.  All Rights Reserved.
+// 
+// @KNOWNOW_LICENSE_START@
+//  
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//  
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in
+// the documentation and/or other materials provided with the
+// distribution.
+//  
+// 3. The name "KnowNow" is a trademark of KnowNow, Inc. and may not
+// be used to endorse or promote any product without prior written
+// permission from KnowNow, Inc.
+//  
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL KNOWNOW, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+// @KNOWNOW_LICENSE_END@
+// 
+// $Id: index_ja.js,v 1.1 2003/03/22 04:06:24 ifindkarma Exp $
+// 
+// 
+// sample Japanese localizations, thanks to AltaVista's Babelfish
+//
+kn_localize("ja","",
+"Bid",
+kn_unescape("%E4%BB%98%E3%81%91%E5%80%A4"));
+kn_localize("ja","",
+"Ask",
+kn_unescape("%E8%A8%80%E3%81%84%E5%80%A4"));
+kn_localize("ja","",
+"Volume",
+kn_unescape("%E5%A3%B2%E8%B2%B7%E9%AB%98"));
+kn_localize("ja","",
+"Last",
+kn_unescape("%E6%9C%80%E8%BF%91"));
+kn_localize("ja","",
+"Close",
+kn_unescape("%E5%BC%95%E3%81%91"));
+kn_localize("ja","",
+'font-family: arial, helvetica, sans-serif; font-size: 12px;',
+'font-family: courier, helvetica, sans-serif; font-size: 12px; font-weight: normal;');
+kn_localize("ja","",
+'font-family: arial, helvetica, sans-serif; font-size: 18px;',
+'font-family: arial, helvetica, sans-serif; font-size: 18px;');
+kn_localize("ja","",
+'font-family: arial, helvetica, sans-serif; font-size: 10px;',
+'font-family: arial, helvetica, sans-serif; font-size: 10px;');
+kn_localize("ja","",
+"%{0}'s Stock Portfolio",
+kn_unescape("%25%7B0%7D%E3%81%AE%E3%83%9D%E3%83%BC%E3%83%88%E3%83%95%E3%82%A9%E3%83%AA%E3%82%AA"));
+//kn_localize("ja","",
+//"Ticker Symbol",
+//kn_unescape("%E4%B8%8A%E5%A0%B4%E6%A0%AA"));
+kn_localize("ja","",
+"Ticker Symbol",
+kn_unescape("%E4%BC%9A%E7%A4%BE"));
+kn_localize("ja","",
+"Add",
+kn_unescape("%E6%8C%BF%E5%85%A5%E7%89%A9"));
+kn_localize("ja","",
+"Remove",
+kn_unescape("%E5%89%8A%E9%99%A4"));
+kn_localize("ja","",
+"This page uses delayed data from %{0}.",
+kn_unescape("%E3%81%93%E3%81%AE%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AF%25%7B0%7D%E3%81%8B%E3%82%89%E3%81%AE%E9%81%85%E3%82%89%E3%81%9B%E3%82%89%E3%82%8C%E3%81%9F%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B%E3%80%82"));
+kn_localize("ja","",
+"nasdaq.com",
+null);
+kn_localize("ja","",
+"Prices are in US dollars.",
+kn_unescape("%E4%BE%A1%E6%A0%BC%E3%81%AF%E3%83%89%E3%83%AB%E3%81%AB%E3%81%82%E3%82%8B%E3%80%82"));
+kn_localize("ja","",
+"Stock Portfolio",
+kn_unescape("%E3%83%9D%E3%83%BC%E3%83%88%E3%83%95%E3%82%A9%E3%83%AA%E3%82%AA"));
+// End of index_ja.js
