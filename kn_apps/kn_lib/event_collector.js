229149a602d6aeecb4876fb12b334abae5acdc4c
ifindkarma <ifindkarma>
2003-04-16 03:11:36 +0000

    Added some topic viewing tools and libraries.

---
+++ kn_apps/kn_lib/event_collector.js
@@ -0,0 +1,158 @@
+// PubSub JavaScript Event Collector Library.
+
+// Demo that uses this library: mod_pubsub/kn_apps/eventcollector/
+
+// Copyright 2000-2003 KnowNow, Inc.  All Rights Reserved.
+
+// @KNOWNOW_LICENSE_START@
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in
+// the documentation and/or other materials provided with the
+// distribution.
+// 
+// 3. The name "KnowNow" is a trademark of KnowNow, Inc. and may not
+// be used to endorse or promote any product without prior written
+// permission from KnowNow, Inc.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL KNOWNOW, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// @KNOWNOW_LICENSE_END@
+
+// $Id: event_collector.js,v 1.1 2003/04/16 03:11:37 ifindkarma Exp $
+
+
+// To use event_collector.js, create an instance of PubSubEventCollection
+// and pass it as the second argument to kn.subscribe. The object then
+// maintains a "view" of the events in that topic.  It automatically
+// removes deleted events and overwrites duplicates with any new
+// header values.  The onEvent handler notifies your app of events;
+// it is up to the application developer to decide how to interpret
+// (and render) the results.
+
+// There's also an "apply" method that basically takes a function and
+// applies it to every event in the view.
+
+
+function PubSubEventCollection()
+// Constructor.
+{
+    this.events = [];
+    if (typeof(this.events.item) == "undefined")
+    {
+        this.events.item = PubSubEventCollection.Item;
+    }
+    this.length = 0;
+    this.deletions = false;
+    return this;
+}
+
+// Default empty handler.
+PubSubEventCollection.prototype.onEvent = function(){}
+
+PubSubEventCollection.Item = function(index)
+// Returns event at specified index in the collection.
+// Replacement for item() method absent in most non-IE browsers.
+{
+    if (index < this.events.length - 1)
+    {
+        return this.events[index];
+    }
+    else
+    {
+        return null;
+    }
+}
+
+PubSubEventCollection.prototype.onMessage = function(evt)
+// Receives new events and places them into events[] collection.
+// Identifies duplicates and removes deleted events if requested.
+{
+    var index = this.getIndex(evt);
+    if (index < 0)
+    {
+        this.events[this.events.length] = evt;
+    }
+    else
+    {
+        this.events[index] = evt;
+        if (this.deletions && evt.kn_deleted == "true")
+        {
+            this.remove(evt);
+        }
+    }
+    this.length = this.events.length;
+    // Fire onEvent handler.
+    this.onEvent();
+}
+
+PubSubEventCollection.prototype.remove = function(evt)
+// Removes an event from the events collection.
+// Returns index of removed event.
+{
+    var index = this.getIndex(evt)
+    if (index > -1)
+    {
+        var arry = [];
+        for (var i=0;i<this.events.length;i++)
+        {
+            // Cannot rely on slice and concat, so we just copy
+            // the values into a new array.
+            if (!this.events[i].kn_deleted)
+            {
+                arry[arry.length] = this.events[i];
+            }
+        }
+        this.events = arry;
+        return index;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+PubSubEventCollection.prototype.getIndex = function(evt)
+// Returns the numeric index of this event in the collection.
+{
+    var id = evt.kn_id;
+    for (var i=0; i<this.events.length;i++)
+    {
+        if (this.events[i].kn_id == id)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+PubSubEventCollection.prototype.apply = function(fn)
+// Applies function fn to all events in the collection.
+// Returns array of return values.
+{
+    var arry = [];
+    for (var i=0; i<this.events.length;i++)
+    {
+        arry[i] = fn(this.events[i]);
+    }
+    return arry;
+}
+
+// End of event_collector.js
---
+++ kn_apps/kn_lib/event_list.js
@@ -0,0 +1,157 @@
+// PubSub JavaScript Event List Library.
+
+// Demo that uses this library: mod_pubsub/kn_apps/eventlist/
+
+// Copyright 2000-2003 KnowNow, Inc.  All Rights Reserved.
+
+// @KNOWNOW_LICENSE_START@
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in
+// the documentation and/or other materials provided with the
+// distribution.
+// 
+// 3. The name "KnowNow" is a trademark of KnowNow, Inc. and may not
+// be used to endorse or promote any product without prior written
+// permission from KnowNow, Inc.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL KNOWNOW, INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// @KNOWNOW_LICENSE_END@
+
+// $Id: event_list.js,v 1.1 2003/04/16 03:11:37 ifindkarma Exp $
+
+
+// This is dead-simple but useful script that subscribes an HTML tag
+// to a topic, making anything inside that tag a template for each
+// event that arrives.  In this way, you can easily make a "live"
+// list of events, formatted any way you wish.
+
+// To seed your PubSub Server with some purchase orders, submit to your
+// Web browser's URL:
+
+//     http://mypubsubserver/kn/what/pos?do_method=notify&order_number=1&order_pcs=1000&order_item=Socks
+//     http://mypubsubserver/kn/what/pos?do_method=notify&order_number=2&order_pcs=1001&order_item=Shoes
+
+// Now, if you want to build a live list of purchase orders,
+// you can do something like this:
+
+// <div class="event_list" kn_from="/what/pos" do_max_age="infinity" kn_deletions="true">
+//   <p>
+//     <b>Purchase Order #{{order_number}}</b><br>
+//     Item #: {{item_number}}<br>
+//     Quantity: {{item_quantity}}<br>
+//     Description: {{item_description}}<br>
+//   </p>
+// </div>
+
+// When you load this page in IE, the script will take the innards of
+// the DIV tag and make it a template, replacing the {{braced}} text
+// with the value of the corresponding PubSub header.  It will repeat
+// this process for each event received, creating a formatted list.
+
+
+
+function PubSubEventListInit()
+// Initializes elements for subscriptions.
+{
+    // Get all elements.
+    var elms = document.all;
+    // Loop thru.
+    for (var i=0;i<elms.length;i++)
+    {
+        // Only elements of the event_list class get initialized.
+        if (elms[i].className.indexOf("event_list") != -1)
+        {
+            elms[i].onMessage = doPubSubEventList;
+            // Store template HTML.
+            elms[i]._template = elms[i].innerHTML;
+            elms[i]._evts = [];
+            elms[i].innerHTML = "";
+            kn.subscribe(
+                elms[i].kn_from,
+                elms[i],
+                _pubsub_elm2event(elms[i])
+            );
+        }
+    }
+}
+
+function _pubsub_elm2event(elm)
+// Retrieves the specified request headers from the HTML element.
+{
+    var a = _pubsub_elm2event.lookup;
+    var o = {};
+    for (var i=0;i<a.length;i++)
+    {
+        if (elm[a[i]])
+        {
+            o[a[i]] = elm[a[i]]; 
+        }
+    }
+    return o;
+}
+_pubsub_elm2event.lookup = ["do_max_n","do_max_age","kn_expires","kn_deletions"];
+
+function doPubSubEventList(e)
+// Processes the headers and places them into the correct cells.
+{
+    if (e.kn_deleted)
+    {
+        // If deleted, blank out this event's HTML entry.
+        this._evts[assignIndex(this,e)].html  = "";
+    } else
+    {
+        // Get template for this element.
+        var html = this._template;
+        for (var header in e)
+        {
+            // Replace template tokens with event header values.
+            var regexp = new RegExp("{{" + header.toLowerCase() + "}}", "gi");
+            html = html.replace(regexp,  kn_htmlEscape(e[header]));
+        }
+        // Create new HTML entry.
+        this._evts[assignIndex(this,e)] = {id:e.kn_id,html:html};
+    }
+    
+    // Now concatenate HTML string.
+    var str = "";
+    for (var i=0;i<this._evts.length;i++)
+    {
+        str += this._evts[i].html;
+    }
+    // And insert it into the element. Voila!
+    this.innerHTML = str;
+}
+
+function assignIndex(obj,e)
+// Looks up or assigns index of event object in this elements' array.
+{
+    for (var i=0;i<obj._evts.length;i++)
+    {
+        if (obj._evts[i].id == e.kn_id)
+        {
+            return i;
+        }
+    }
+    return obj._evts.length;
+}
+
+// End of event_list.js
